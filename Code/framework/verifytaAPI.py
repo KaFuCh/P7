21cd8b20c0697ed11eb534c43408785e2afebad8
Mathias Jensen <betaboxguugi@gmail.com>
2016-10-13 11:49:51 +0200

    Refactor of GA

--- Code/framework/verifytaAPI.py
+++ Code/framework/verifytaAPI.py
@@ -2,17 +2,45 @@ import subprocess
 import re
 
 
-def run_verifyta(xml, queries, *args, verifyta='./verifyta'):
+def run_verifyta(xml, queries, *args, verifyta):
     """
     :param xml: string giving the path to a uppaal project XML file
-    :param queries: string giving the path to a uppaal query XML file
-    :param args: other args giving to verifyta, e.g. -t 2 for getting the fastest trace.
+    :param queries: string giving the path to a uppaal query file
+    :param *args: other args giving to verifyta, e.g. -t 2 for getting the fastest trace.
     :param verifyta: string giving the path to verifyta
     :return 0: Returns the standard output, i.e. if the queries were satisfied
     :return 1: Returns the trace(s) of the queries.
     """
     res = subprocess.run([verifyta, xml, queries] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return res.stdout, res.stderr   # Result, Trace
+    return res.stdout, res.stderr   # ResultBin, Trace
+
+
+def run_verifyta_some_breadth(xml, queries, verifyta):
+    """
+    :param xml: string giving the path to a uppaal project XML file
+    :param queries: string giving the path to a uppaal query file
+    :param verifyta: string giving the path to verifyta
+    :return 0: Returns the standard output, i.e. if the queries were satisfied
+    :return 1: Returns the trace(s) of the queries.
+    """
+    # -o 0: Argument for Breadth First search
+    # -t 0: Argument for finding some trace
+    result, trace = run_verifyta(xml, queries, '-o 0', '-t 0', verifyta)
+    return result, trace
+
+
+def run_verifyta_best_randbestdepth(xml, queries, verifyta):
+    """
+    :param xml: string giving the path to a uppaal project XML file
+    :param queries: string giving the path to a uppaal query file
+    :param verifyta: string giving the path to verifyta
+    :return 0: Returns the standard output, i.e. if the queries were satisfied
+    :return 1: Returns the trace(s) of the queries.
+    """
+    # -o 4: Argument for Random Best Depth First
+    # -t 3: Argument for finding Best Trace
+    result, trace = run_verifyta(xml, queries, '-o 4', '-t 3', verifyta)
+    return result, trace
 
 
 def get_trace_time(trace, clock_name='global_c'):
@@ -33,10 +61,25 @@ def get_best_cost(result, default):
     :return: An integer representing the best cost
     """
     lst = result.splitlines()
-    if len(lst) == 13:
+    if len(lst) == 13:      # If the length is not 13, we know the verification failed
         s = str(lst[-2])    # The information we want is on the second last line
         res = re.search(' -- Best solution   : (\d+)', s).group(1)
     else:
         res = default
     return int(res)
+
+
+def get_satisfied(result):
+    """
+    :param result: binary string with a result from UPPAAL CORA
+    :return: Boolean, False if some query did not succeed, True if all queries succeeded
+    """
+    s = str(result)
+    if re.search('-- Property is NOT satisfied.', s):
+        return False
+    else:
+        return True
+
+
+
 
